8a22ae6946ee174dabf0e7dc455f90f6b203a01e
Ahmedaligue <158603161+Ahmedaligue@users.noreply.github.com>
2024-07-03 03:16:35 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import fetch from 'node-fetch';
 import axios from 'axios';
 import moment from 'moment-timezone';
 
-global.botnumber = ""
+global.botnumber = "212713542982"
 
 global.owner = [
   [212658594530', 'ahmed aligue', true],
